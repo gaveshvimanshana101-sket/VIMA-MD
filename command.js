e297eaefa39146d1448f803cfb43b8f43c12a295
gaveshvimanshana101-sket <gaveshvimanshana101@gmail.com>
2026-03-06 21:53:43 +0530

    Update command.js

--- command.js
+++ command.js
@@ -1,64 +1,52 @@
 const commands = [];
 const replyHandlers = [];
 
-// COMMAND REGISTER FUNCTION
-function cmd(options) {
-  commands.push(options);
-}
+module.exports = { commands, replyHandlers };
 
-module.exports = {
-  commands,
-  replyHandlers,
-  cmd
-};
-
-/* ===============================
-   COMMANDS
-================================*/
 
 // PING COMMAND
-cmd({
-  pattern: "ping",
-  alias: ["speed"],
-  react: "⚡",
-  function: async (vima, mek, m, { reply }) => {
+commands.push({
+pattern: "ping",
+alias: ["speed"],
+react: "⚡",
+function: async (vima, mek, m, { reply }) => {
 
-    reply("🏓 Pong! Bot is working perfectly.");
+reply("🏓 Pong! Bot is working.");
 
-  }
+}
 });
+
 
 // OWNER COMMAND
-cmd({
-  pattern: "owner",
-  react: "👑",
-  function: async (vima, mek, m, { reply }) => {
+commands.push({
+pattern: "owner",
+react: "👑",
+function: async (vima, mek, m, { reply }) => {
 
-    reply(`
-👑 *BOT OWNER*
+reply(`👑 *BOT OWNER*
 
 Name : Vima
-Number : 94789706579
-`);
+Number : 94789706579`);
 
-  }
+}
 });
 
-// MENU COMMAND
-cmd({
-  pattern: "menu",
-  react: "📜",
-  function: async (vima, mek, m, { reply }) => {
 
-    reply(`
-╭━━━〔 🤖 VIMA-MD MENU 〕━━━╮
+// MENU COMMAND
+commands.push({
+pattern: "menu",
+react: "📜",
+function: async (vima, mek, m, { reply }) => {
+
+reply(`
+╭━━〔 VIMA-MD MENU 〕━━╮
 
 ⚡ .ping
 👑 .owner
 📜 .menu
 
-╰━━━━━━━━━━━━━━━━━━╯
+╰━━━━━━━━━━━━━━╯
 `);
 
-  }
+}
 });
