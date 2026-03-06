3e6a6e8f4b2084f26509ce992a991dc4aa8846a6
gaveshvimanshana101-sket <gaveshvimanshana101@gmail.com>
2026-03-06 12:32:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "PRABATH-MD~vDAlWr6jcuCkH81", // Put your session id here
+  SESSION_ID: "RBsTHArL#NHCQT5yzQw6gFeWSJvUKncwqryqDfptWcLZ9DajMFXA", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
   ALIVE_MSG: "*Hello👋 VIMA-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
