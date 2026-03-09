b36ec3d0445fe52545a7f12eda81e08e93f5fe03
gaveshvimanshana101-sket <gaveshvimanshana101@gmail.com>
2026-03-09 07:30:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RBsTHArL#NHCQT5yzQw6gFeWSJvUKncwqryqDfptWcLZ9DajMFXA",
+SESSION_ID: process.env.SESSION_ID || "5I8iXSaK#4ZKlIPS1j_GyFt9Hn5oZ62tuIokYnz5lYxrrrr-buvA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIMA-MD Is Alive Now😍*",
 BOT_OWNER: '94789706579',  // Replace with the owner's phone number
